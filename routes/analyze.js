8f245d7dd4dfcee12d0f192a53923a76ecbef1a6
Ruslan Gainanov <gromrx1@gmail.com>
2016-03-19 14:09:14 +0300

    Add implementation frequency stems. Add Tanenbaum text, stop word list. Win8

--- routes/analyze.js
+++ routes/analyze.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var natural = require('natural');
+var tokenizer1 = require('Tokenizer');
 var isCyrillic = function (text) {
     return /[а-яА-Я]/i.test(text);
 };
@@ -58,6 +59,107 @@ function getMostFreqTokens(A){
     //console.log('getMostFreqTokens #' + out);
     return res;
 }
+//var _id=0;
+//function DictStem( stem ) {
+//    this.id = ++_id;
+//    this.stem = stem;
+//    this.get = function () {
+//        return this.id + " " + this.stem;
+//    };
+//}
+
+function ConceptAndStemValue(c1, s1, n1 ){
+    this.idConc=c1;
+    this.idStem=s1;
+    this.numbStems=n1;
+    this.set = function (c, s, n) {
+        this.idConc = c;
+        this.idStem = s;
+        this.numbStems = n;
+    }
+}
+
+// **************** ConceptAndStemTable **********************
+function ConceptAndStemTable( ){
+    this.table=[];
+    this.add = function (c, s, n) {
+        var value = new ConceptAndStemValue(c, s, n);
+        this.table.push(value);
+        return this.table.length-1;
+    };
+    this.getLength = function () {
+        return this.table.length;
+    };
+    this.print = function () {
+        var arr=[];
+        for(var i=0; i<this.table.length; i++){
+            arr.push(i+"\t=>\t"+this.table[i].idConc+" \t "+this.table[i].idStem);
+        }
+        return arr.join('\n');
+    }
+}
+
+// **************** DictConcepts **********************
+function DictConcepts( ){
+    this.concepts=new Array();
+    this.addConcept = function (c) {
+        function checkAvailability(arr, val) {
+            return arr.some(function(arrVal) {
+                return val === arrVal;
+            });
+        }
+        if(!checkAvailability(this.concepts, c)) {
+            //если отсутсвует
+            this.concepts.push(c);
+            return this.concepts.length-1;
+        }
+        else {
+            return this.concepts.indexOf(c);
+        }
+    };
+    this.getLength = function () {
+        return this.concepts.length;
+    };
+    this.print = function () {
+        var arr=[];
+        for(var i=0; i<this.concepts.length; i++){
+            arr.push(i+"\t=>\t"+this.concepts[i]);
+        }
+        return arr.join('\n');
+    }
+}
+// **************** DictStems **********************
+//класс для хранения словарных стем
+function DictStems(  ) {
+    this.stems=new Array();
+    this.addStem = function (stem) {
+        function checkAvailability(arr, val) {
+            return arr.some(function(arrVal) {
+                return val === arrVal;
+            });
+        }
+        if(!checkAvailability(this.stems, stem)) {
+            //если отсутсвует
+            this.stems.push(stem);
+            return this.stems.length-1;
+        }
+        else {
+            return this.stems.indexOf(stem);
+        }
+    };
+    this.getLength = function () {
+        return this.stems.length;
+    };
+    this.print = function () {
+        var arr=[];
+        for(var i=0; i<this.stems.length; i++){
+            arr.push(i+"\t=>\t"+this.stems[i]);
+        }
+        return arr.join('\n');
+        //return this.stems.join(';');
+    }
+}
+
 stemmer = natural.PorterStemmerRu;
 //stemmer.attach();
 stemmerEng = natural.PorterStemmer;
@@ -66,32 +168,79 @@ TfIdf = natural.TfIdf;
 tfidf = new TfIdf();
 tokenizer = new natural.AggressiveTokenizerRu();
 fs = require('fs');
-var filename = '/!Code/Node/FirstApp1/etc/files/dict_all.txt';
+var filename = '/!Code/Node/FirstApp1/etc/files/tanen_index_all_mod_new.txt';
 var stopWordRuFile = '/!Code/Node/FirstApp1/etc/files/stop_ru.txt';
 var stopWordEngFile = '/!Code/Node/FirstApp1/etc/files/stop_eng.txt';
 var data = fs.readFileSync(filename,'utf8');
 //console.log (data);
 var dict1 = data.toString().split('\n');
 console.log(dict1.length);
+var myDict1Stems = new DictStems();
+var myDictConcepts = new DictConcepts();
+var myConceptAndStemTable = new ConceptAndStemTable();
+console.log( "myDict1Stems.getLength()=" + myDict1Stems.getLength() );
+//myDict1Stems.addStem('A');
+//myDict1Stems.addStem('B');
+//myDict1Stems.addStem('C');
+//myDict1Stems.addStem('A');
+//console.log( "myDict1Stems.getLength()=" + myDict1Stems.getLength() );
+//console.log( "myDict1Stems=" + myDict1Stems.stems.reduce(function(sum, current) { return sum + current; }, '' ) );
+//console.log( "myDict1Stems=" + myDict1Stems.print() );
 var dict1Stems = dict1.map(function(item) {
-    if(item.split(' ').length==1){
-        if(isCyrillic(item))
-            return  stemmer.stem(item);
-        else
-            return  stemmerEng.stem(item);
+    var idConc = myDictConcepts.addConcept(item);
+    var idStem;
+    var stem;
+    var words1=item.split(' ');
+    if(words1.length==1){
+        if(isCyrillic(item)) {
+            idStem= myDict1Stems.addStem(stemmer.stem(item));
+            stem= stemmer.stem(item);
+        }
+        else {
+            idStem= myDict1Stems.addStem(stemmerEng.stem(item));
+            stem=  stemmerEng.stem(item);
+        }
+        myConceptAndStemTable.add(idConc, idStem, 1);
     }
-    else
-        return '';
+    else {
+        for(var i=0; i<words1.length; i++)
+        {
+            var word=words1[i];
+            if(isCyrillic(word)) {
+                idStem= myDict1Stems.addStem(stemmer.stem(word));
+            }
+            else {
+                idStem= myDict1Stems.addStem(stemmerEng.stem(word));
+            }
+            myConceptAndStemTable.add(idConc, idStem, words1.length);
+        }
+        stem= '';
+    }
+
+    return stem;
 });
+//console.log('Dict1Stem: ');
+//console.log(dict1Stems);
 console.log('Dict1Stem count:');
 console.log(dict1Stems.length);
+console.log( "myDict1Stems.getLength()=" + myDict1Stems.getLength() );
+console.log( "myDict1Stems=" + myDict1Stems.print() );
+console.log( "myDictConcepts.getLength()=" + myDictConcepts.getLength() );
+console.log( "myDictConcepts=" + myDictConcepts.print() );
+console.log( "myConceptAndStemTable.getLength()=" + myConceptAndStemTable.getLength() );
+console.log( "myConceptAndStemTable=" + myConceptAndStemTable.print() );
+
 
 var stopWordRu = fs.readFileSync(stopWordRuFile,'utf8').toString().split('\n');
 var stopWordEng = fs.readFileSync(stopWordEngFile,'utf8').toString().split('\n');
+//console.log('stopWordRu count:');
+//console.log(stopWordRu.length);
+//console.log(stopWordRu.join('\n'));
 
 //router.get('/', function(req, res, next) {
 //    res.render('index', { title: 'Express' });
 //});
+
 
 router.get('/', function(req, res){
     var query = require('url').parse(req.url,true).query;
@@ -102,15 +251,23 @@ router.get('/', function(req, res){
 });
 
 router.post('/', function(req, res){
-    var textarea1 = req.body.textarea1;
+    //var textarea1 = req.body.textarea1;
+    var textarea1 = fs.readFileSync('/!Code/Node/FirstApp1/etc/files/tanen_part2_process_and_threads_new.txt','utf8').toString();
     //res.send('POST: ' + JSON.stringify(req.body) + '<br>'+textarea1);
     console.log('POST: '+ textarea1);
     //var taTokens = textarea1.tokenizeAndStem(true);
     //console.log(taTokens);
-    var taTokens2 =  tokenizer.tokenize(textarea1);
-    //console.log(taTokens2.length);
-    taTokens2=diff(diff(taTokens2.map(function(t) { return t.toLowerCase(); }),stopWordEng), stopWordRu);
-    //console.log(taTokens2.length);
+    //var taTokens2 =  tokenizer.tokenize(textarea1);//from natural module
+    var taTokens2 =  tokenizer1(textarea1);
+    console.log("taTokens2.length="+taTokens2.length);
+    //исключение стоп-слов из списка токенов (выполнение разности с двумя массивами
+    //taTokens2=diff(diff(taTokens2, stopWordEng), stopWordRu);
+    //taTokens2=diff(taTokens2, stopWordRu);
+
+    //исключение стоп-слов из списка токенов (выполнение разности с двумя массивами)
+    //не работает: убирает слишком много слов - из 43тыс оставил только 7тыс
+    //taTokens2=diff(diff(taTokens2.map(function(t) { return t.toLowerCase(); }),stopWordEng), stopWordRu);
+    console.log("taTokens2.length="+taTokens2.length);
     for (var i = 0; i < taTokens2.length; i++) {
         if(isCyrillic(taTokens2[i]))
             taTokens2[i] = stemmer.stem(taTokens2[i]);
@@ -118,9 +275,15 @@ router.post('/', function(req, res){
             taTokens2[i] = stemmerEng.stem(taTokens2[i]);
     }
     //console.log('Tokens: ' + taTokens2);
-    var freqsTokens = getMostFreqTokens(taTokens2);
-    console.log("Freqs: "+freqsTokens.length + ": "+ freqsTokens);
+    //var freqsTokens = getMostFreqTokens(taTokens2);
+    var freqsTokens = taTokens2;
+    //console.log("Freqs: "+freqsTokens.length + ": "+ freqsTokens);
     var keys = intersection(freqsTokens, dict1Stems);
+    var myKeys = intersection(freqsTokens, myDict1Stems.stems);
+    console.log('keys.length= ' + keys.length);
+    console.log('myKeys.length= ' + myKeys.length);
+
+
     var keysOrig=[];
     var prev=null;
     for (var i = 0; i < dict1Stems.length; i++) {
@@ -154,6 +317,29 @@ router.post('/', function(req, res){
         }
     }
     console.log('Lenght key orig: ' + keysOrig.length);
+    var keysAndFreqs=new Array(keys.length);
+    for(var i = 0; i < keys.length; i++) {
+        keysAndFreqs[i]=new Array(2);
+        keysAndFreqs[i][0]=keys[i];
+        var indices = 0;
+        var element = keys[i];
+        var idx = freqsTokens.indexOf(element);
+        while (idx != -1) {
+            indices=indices+1;
+            idx = freqsTokens.indexOf(element, idx + 1);
+        }
+        keysAndFreqs[i][1]=indices;
+        //console.log(keys[i]+"->"+indices);
+    }
+    //console.log(keysAndFreqs.join('\n'));
+    // Сортировка по частоте
+    keysAndFreqs.sort(function(a, b) {
+        if (a[1] < b[1]) return 1;
+        else if (a[1] > b[1]) return -1;
+        else return 0;
+    });
+    //console.log(keysAndFreqs.join('\n'));
+
     /*
     console.log('SORT: ');
     taTokens.sort();
@@ -199,7 +385,9 @@ router.post('/', function(req, res){
         //pContent: taTokens2,
         text1: textarea1,
         pKeys: keys,
-        pKeysOrig: keysOrig
+        pKeysOrig: keysOrig,
+        pKeysAndFreq: keysAndFreqs,
+        tokenText1: taTokens2
     });
 });
 
